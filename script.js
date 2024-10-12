bb1e6e4e16d10daa1908cce1271ab90e5c51b2fe
Anil Kumar <avulanil17@gmail.com>
2024-10-12 21:16:06 +0530

    updated cards list now it can take songs for the local storage

--- script.js
+++ script.js
@@ -1,17 +1,19 @@
 let curSong = new Audio();
 let songs = []
 let isPause;
-async function getSongs() {
-    let a = await fetch("http://127.0.0.1:5500/Spotify-clone/songs/");
+let curFolder;
+async function getSongs(folder) {
+    curFolder = `http://127.0.0.1:5500/Spotify-clone/${folder}/`;
+    let a = await fetch(`http://127.0.0.1:5500/Spotify-clone/${folder}/`);
     let response = await a.text();
     let div = document.createElement("div");
     div.innerHTML = response;
     let as = div.getElementsByTagName("a");
-    // let songs = [];
+    songs = [];
     for (let i = 0; i < as.length; i++) {
         let element = as[i];
-        if (element.href.endsWith("mp3")) {
-            songs.push(element.href.split("/songs/")[1]);
+        if (element.href.endsWith(".mp3")) {
+            songs.push(element.href.split(`/${folder}/`)[1]);
         }
     }
 
@@ -44,7 +46,6 @@ function previousSong() {
     }
 }
 
-// function timeUpdate ()
 
 
 function playPauseMusic() {
@@ -67,7 +68,7 @@ function playNextSong() {
 }
 
 function playMusic(track, pause = false) {
-    curSong.src = "http://127.0.0.1:5500/Spotify-clone/songs/" + track;
+    curSong.src = `${curFolder}/` + track;
     if (!pause) {
         curSong.play();
         document.querySelector(".playSong").src = "Images/pause.svg"
@@ -77,5 +78,72 @@ function playMusic(track, pause = false) {
     document.querySelector(".time").textContent = "00:00/00:00";
 }
 
+function secondsToMinutesSeconds(seconds) {
+    if (isNaN(seconds) || seconds < 0) {
+        return "00:00";
+    }
 
-getSongs()
+    const minutes = Math.floor(seconds / 60);
+    const remainingSeconds = Math.floor(seconds % 60);
+
+    const formattedMinutes = String(minutes).padStart(2, '0');
+    const formattedSeconds = String(remainingSeconds).padStart(2, '0');
+
+    return `${formattedMinutes}:${formattedSeconds}`;
+}
+
+curSong.addEventListener ("timeupdate", ()=> {
+    document.querySelector (".time").innerHTML = `${secondsToMinutesSeconds (curSong.duration)} / ${secondsToMinutesSeconds (curSong.currentTime)}`;
+    document.querySelector (".circle").style.left = (curSong.currentTime/curSong.duration) * 100 + "%";
+})
+
+document.addEventListener ("DOMContentLoaded", function () {
+    document.querySelector("#seekBar").addEventListener("click", e => {
+        let percent = (e.offsetX / e.target.getBoundingClientRect().width) * 100;
+        document.querySelector(".circle").style.left = percent + "%";
+        currentSong.currentTime = ((curSong.duration) * percent) / 100;
+    })
+})
+
+// getSongs ("songs/Devara")
+function loadSongs () {
+    Array.from (document.getElementsByClassName ('card')).forEach (e=> {
+        e.addEventListener ('click', async (item)=> {
+            await getSongs (`songs/${item.currentTarget.dataset.folder}`)
+        })
+    })
+}
+
+async function displayAlbums () {
+    let a = await fetch(`http://127.0.0.1:5500/Spotify-clone/songs/`);
+    let response = await a.text();
+    let div = document.createElement("div");
+    div.innerHTML = response;
+    let as = div.getElementsByTagName("a");
+    let array = Array.from (as);
+    let cardContainer = document.querySelector (".cardList");
+    for (let i=0; i<array.length; i++) {
+        let element = array[i];
+        if (element.href.includes ("/songs/")) {
+            let folder = element.href.split ("songs/").slice (-1);
+            let a = await fetch (`http://127.0.0.1:5500/Spotify-clone/songs/${folder}/info.json`);
+            let response = await a.json ();
+            cardContainer.innerHTML = cardContainer.innerHTML + `<div class="card" data-folder="${folder}" onclick="loadSongs ()">
+              <img
+                src="/songs/${folder}/cover.jpg"
+                width="200"
+                height="200"
+                alt=""
+              />
+              <h1>${response.title}</h1>
+              <h4>${response.description}</h4>
+            </div>`
+        }
+    }
+
+}
+async function main () {
+    await displayAlbums  ();
+}
+
+main ();
